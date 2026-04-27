f2a5709111d3d8afce0f4ef2c617e8b57218814f
san-r1 <diassanti36@gmail.com>
2026-04-27 16:28:42 -0600

    cliente
    cliente

---
+++ client.ts
@@ -0,0 +1,30 @@
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+const program = anchor.workspace.GestorTareas;
+
+async function main() {
+  console.log("Iniciando...");
+
+  const user = provider.wallet.publicKey;
+
+  const [tareaPubkey] = web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("tarea"), user.toBuffer()],
+    program.programId
+  );
+
+  console.log("Creando tarea...");
+
+  const tx = await program.methods
+    .crearTarea("Hola Solana! 🚀")
+    .accounts({
+      tarea: tareaPubkey,
+      usuario: user,
+      systemProgram: web3.SystemProgram.programId,
+    })
+    .rpc();
+
+  console.log("✅ LISTO!", tx);
+}
+
+main();
