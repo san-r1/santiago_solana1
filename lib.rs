dea2ff265edd31b71d33b1521423eaa9ff3bdf86
san-r1 <diassanti36@gmail.com>
2026-04-27 16:32:04 -0600

    src
    src

---
+++ lib.rs
@@ -0,0 +1,95 @@
+use anchor_lang::prelude::*;
+
+declare_id!("5eApey6earZK4UUaVMT8RfKUTFuf8Mm8kwSMsjNXW2bs");
+
+#[program]
+pub mod gestor_tareas {
+    use super::*;
+
+    pub fn crear_tarea(
+        ctx: Context<CrearTarea>,
+        contenido: String
+    ) -> Result<()> {
+        let tarea = &mut ctx.accounts.tarea;
+
+        tarea.contenido_del = contenido;
+        tarea.completada = false;
+        tarea.usuario = ctx.accounts.usuario.key();
+
+        Ok(())
+    }
+
+    pub fn completar_tarea(
+        ctx: Context<CompletarTarea>
+    ) -> Result<()> {
+        let tarea = &mut ctx.accounts.tarea;
+
+        tarea.completada = true;
+
+        Ok(())
+    }
+
+    pub fn eliminar_tarea(
+        _ctx: Context<EliminarTarea>
+    ) -> Result<()> {
+        Ok(())
+    }
+}
+
+// ---------------------------------------------------------------------
+// CONTEXTOS
+// ---------------------------------------------------------------------
+
+#[derive(Accounts)]
+pub struct CrearTarea<'info> {
+    #[account(
+        init,
+        payer = usuario,
+        space = 8 + 32 + 4 + 200 + 1, // <-- CORREGIDO: Espacio fijo
+        seeds = [b"tarea", usuario.key().as_ref()],
+        bump
+    )]
+    pub tarea: Account<'info, Tarea>,
+
+    #[account(mut)]
+    pub usuario: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CompletarTarea<'info> {
+    #[account(
+        mut,
+        seeds = [b"tarea", usuario.key().as_ref()],
+        bump
+    )]
+    pub tarea: Account<'info, Tarea>,
+
+    pub usuario: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct EliminarTarea<'info> {
+    #[account(
+        mut,
+        close = usuario,
+        seeds = [b"tarea", usuario.key().as_ref()],
+        bump
+    )]
+    pub tarea: Account<'info, Tarea>,
+
+    #[account(mut)]
+    pub usuario: Signer<'info>,
+}
+
+// ---------------------------------------------------------------------
+// CUENTA / ESTRUCTURA
+// ---------------------------------------------------------------------
+
+#[account]
+pub struct Tarea {
+    pub usuario: Pubkey,
+    pub contenido_del: String,
+    pub completada: bool,
+}
